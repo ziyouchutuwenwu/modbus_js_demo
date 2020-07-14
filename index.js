e5ca455e67884fa8c636d58c9e5301a8a27b0937
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2020-07-14 19:38:49 +0800

    更新tcp

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // 依赖库 https://www.npmjs.com/package/modbus-serial
 
-var rtu_master = require("./rtu_master");
-rtu_master.demo();
+// var rtu_master = require("./rtu_master");
+// rtu_master.demo();
 
-// var tcp_master = require("./tcp_master");
-// tcp_master.demo();
+var tcp_master = require("./tcp_master");
+tcp_master.demo();
 
 // var udp_master = require("./udp_master");
 // udp_master.demo();
--- tcp_master.js
+++ tcp_master.js
@@ -1,43 +1,71 @@
-function main() {
-  var ModbusRTU = require("modbus-serial");
-  var modbusTCP = new ModbusRTU();
-  modbusTCP.isDebugEnabled = true;
+var ModbusRTU = require("modbus-serial");
+var modbusTCP = new ModbusRTU();
+modbusTCP.isDebugEnabled = true;
 
+function connect() {
   modbusTCP
     .connectTCP("192.168.88.148", { port: 502 })
     .then(function () {
-      info = "Connected, wait for reading...";
-      console.log(info);
-
-      if (!modbusTCP.isOpen) {
-        console.log("client not opened, now close");
-        modbusTCP.close();
-      }
-
-      modbusTCP.setID(1);
-
-      // 从0号地址写入11 22 33 44 55
-      // modbusRTU.writeRegisters(0, [11, 22, 33, 44, 55]).then(readModbusData);
-      modbusTCP
-        .writeRegisters(0, [111, 222, 333, 444, 555])
-        .then(readModbusData);
+      onConnected();
     })
     .catch(function (e) {
-      console.log("exception on connect");
+      console.log("connect failed");
     });
-
-  var readModbusData = function () {
-    modbusTCP
-      .readHoldingRegisters(0, 5)
-      .then(function (data) {
-        mbsStatus = "success";
-        console.log(data);
-        modbusTCP.close();
-      })
-      .catch(function (e) {
-        console.log("exception on reading, ", e);
-      });
-  };
 }
 
-exports.demo = main;
+function readDemo() {
+  modbusTCP
+    .readHoldingRegisters(0, 5)
+    .then(function (data) {
+      mbsStatus = "success";
+      console.log(data);
+    })
+    .catch(function (e) {
+      console.log("exception on reading, ", e);
+    });
+}
+
+function onLoopRead() {
+  if (!modbusTCP.isOpen) {
+    console.log("client not opened");
+    connect();
+  } else {
+    // readDemo();
+  }
+}
+
+function onLoopWrite() {
+  if (modbusTCP.isOpen) {
+    // 从0号地址写入11 22 33 44 55
+    // modbusTCP.writeRegisters(0, [11, 22, 33, 44, 55]).then(readData);
+    // modbusTCP.writeRegisters(0, [11, 22, 33, 44, 66]);
+
+    // 批量写线圈
+    // modbusTCP.writeCoils(0, [true, false, true, true]);
+    // 单个写线圈
+    // modbusTCP.writeCoil(1, true);
+
+    // holding regs
+    // modbusTCP.writeRegisters(2, [10, 9, 8, 11, 33]);
+
+    modbusTCP.writeRegisters(1, [100, 90, 80, 11, 22]);
+
+    console.log("data sent");
+  }
+}
+
+function onConnected() {
+  info = "Connected, wait for reading...";
+  console.log(info);
+
+  if (!modbusTCP.isOpen) {
+    console.log("client not opened, now will exit");
+    modbusTCP.close();
+  }
+
+  modbusTCP.setID(1);
+  setInterval(onLoopRead, 1000);
+  setInterval(onLoopWrite, 10000);
+}
+
+exports.demo = connect;
